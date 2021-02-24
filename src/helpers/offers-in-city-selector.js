8067dfd695ed19f2b4c51ebe7a4b4ba68fc9fa9e
bronskih <dbronskih@mail.ru>
2021-02-24 22:59:12 +0500

    Контроль и ограничения (часть 2)

---
+++ src/helpers/offers-in-city-selector.js
@@ -0,0 +1,22 @@
+import {SortOrders} from './constants';
+
+const offersInCitySelector = ({offers, currentCityName, sortOrder}) => {
+  offers = offers.filter(
+      ({city: {name}}) => name === currentCityName
+  );
+
+  switch (sortOrder) {
+    case SortOrders.RATING:
+      offers.sort((a, b) => b.rating - a.rating);
+      break;
+    case SortOrders.PRICE_UP:
+      offers.sort((a, b) => a.price - b.price);
+      break;
+    case SortOrders.PRICE_DOWN:
+      offers.sort((a, b) => b.price - a.price);
+      break;
+  }
+  return offers;
+};
+
+export default offersInCitySelector;
